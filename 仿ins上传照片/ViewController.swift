d6048bd50cf1065491ce45a1aca1a4cd4ffeebde
shanyazhou <754123815@qq.com>
2018-05-25 16:24:12 +0800

    Initial Commit

---
+++ 仿ins上传照片/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  仿ins上传照片
+//
+//  Created by lilida on 2018/5/25.
+//  Copyright © 2018年 shanyazhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
